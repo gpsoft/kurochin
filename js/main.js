b098ec5bdc68754d815a277cbec92a9f5ecc8947
gpsoft <etoh.gpsoft@gmail.com>
2021-04-21 14:49:27 +0900

    Show roadmap to programmer(full)

--- js/main.js
+++ js/main.js
@@ -14,55 +14,121 @@ function run() {
 			h += 10;
 
 			var color = '#fff';
-			if ( id === '' || id === '1' ) {
+			let bgColors = ['#ff0000', '#ff0000', '#f64343',
+				'#90c8ff', '#90ffbe', '#ffec90', '#ff0000', '#ffec90'];
+			let bgColor = bgColors[type-1];
+			if ( type == 1 || type == 2 || type == 7 ) {
 				w += 10;
 				h += 20;
-				chin.fillOval(ctx2d, pt, w, h, '#ff0000');
+				chin.fillOval(ctx2d, pt, w, h, bgColor);
+			} else if ( type == 3 ) {
+				chin.fillRect(ctx2d, pt, w, h, bgColor);
 			} else {
 				let level = (id.match(/\-/g) || []).length;
-				if ( level === 0 ) {
-					chin.fillRect(ctx2d, pt, w, h, '#f64343');
-				} else {
-					color = '#1f1f1f';
-					chin.fillRRect(ctx2d, pt, w, h,
-						type===1?'#90c8ff':'#90ffbe');
-				}
+				color = '#1f1f1f';
+				chin.fillRRect(ctx2d, pt, w, h, bgColor);
 			}
 			chin.drawText(ctx2d, pt, caption, color);
 		},
 	});
 
-	let data = {
-		nodes: [
-			{id: '', caption: "アプリケーション\nプログラマーに\n必要なスキル"},
-			{id: '1', caption: "共通"},
-			{id: '2', caption: "コマンドライン\nアプリの場合"},
-			{id: '3', caption: "デスクトップ\nアプリの場合"},
-			{id: '4', caption: "モバイル\nアプリの場合"},
-			{id: '5', caption: "Webアプリの場合"},
-			{id: '1-1', type: 1, caption: "パソコン, OS"},
-			{id: '1-2', type: 1, caption: "テキストエディタ"},
-			{id: '1-3', type: 1, caption: "ターミナル、シェル"},
-			{id: '1-4', type: 1, caption: "Git, GitHub"},
-			{id: '1-5', type: 1, caption: "Docker"},
-			{id: '1-6', type: 1, caption: "Virtual Box"},
-			{id: '1-7', type: 2, caption: "コンピュータの仕組み\nプログラムが動く仕組み"},
-			{id: '1-8', type: 2, caption: "OSの仕組み"},
-			{id: '1-9', type: 2, caption: "ネットワークの仕組み"},
-			{id: '1-10', type: 2, caption: "文字コード"},
-			{id: '1-11', type: 2, caption: "データ構造"},
-		],
-		edges: [
-			['', '1'], ['', '2'], ['', '3'], ['', '4'], ['', '5'],
-			['1', '1-1'], ['1', '1-2'], ['1', '1-3'], ['1', '1-4'],
-			['1', '1-5'], ['1', '1-6'],
-			['1', '1-7'],
-			['1', '1-8'],
-			['1', '1-9'],
-			['1', '1-10'],
-			['1', '1-11'],
-		],
+	let trees = [
+		{id: 1, type: 1, caption: "アプリケーション\nプログラマーに\n必要なスキル",
+			children: [
+				{
+					id: 1, type: 3, caption: "必修科目",
+					children: [
+						{id: 1, type: 4, caption: "パソコン, OS"},
+						{id: 2, type: 4, caption: "テキストエディタ"},
+						{id: 3, type: 4, caption: "ターミナル、シェル"},
+						{id: 4, type: 4, caption: "Git, GitHub"},
+						{id: 5, type: 4, caption: "Docker"},
+						{id: 6, type: 4, caption: "Virtual Box"},
+						{id: 7, type: 5, caption: "コンピュータの仕組み\nプログラムが動く仕組み"},
+						{id: 8, type: 5, caption: "OSの仕組み"},
+						{id: 9, type: 5, caption: "ネットワークの仕組み"},
+						{id: 10, type: 5, caption: "文字コード"},
+						{id: 11, type: 5, caption: "データ構造"},
+						{id: 12, type: 5, caption: "正規表現"},
+						{id: 13, type: 5, caption: "暗号、PKI"},
+						{id: 14, type: 5, caption: "オブジェクト指向"},
+						{id: 15, type: 5, caption: "関数型プログラミング"},
+						{id: 16, type: 5, caption: "MVC"},
+						{id: 17, type: 5, caption: "デザインパターン"},
+						{id: 18, type: 5, caption: "外部依存"},
+						{id: 19, type: 5, caption: "ビルド管理\nパッケージ管理"},
+						{id: 20, type: 5, caption: "データ記法"},
+						{id: 21, type: 5, caption: "デバッグ手法"},
+						{id: 22, type: 5, caption: "キャッシュ"},
+						{id: 23, type: 6, caption: "英語"},
+						{id: 24, type: 6, caption: "タッチタイピング"},
+					]},
+				{id: 2, type: 3, caption: "コマンドライン\nアプリの場合",
+					children: [
+						{id: 1, type: 6, caption: "シェルプログラミング"},
+						{id: 2, type: 4, caption: "各種コマンドライン\nユーティリティ"},
+					]},
+				{id: 3, type: 3, caption: "デスクトップ\nアプリの場合",
+					children: [
+						{id: 1, type: 4, caption: "IDE"},
+						{id: 2, type: 5, caption: "ウィンドウマネージャ"},
+						{id: 3, type: 5, caption: "イベントループ"},
+						{id: 4, type: 6, caption: "Electron"},
+						{id: 5, type: 6, caption: "GUIライブラリ"},
+					]},
+				{id: 4, type: 3, caption: "モバイル\nアプリの場合",
+					children: [
+						{id: 1, type: 4, caption: "IDE"},
+						{id: 2, type: 5, caption: "イベントループ"},
+						{id: 3, type: 6, caption: "React Native"},
+					]},
+				{id: 5, type: 3, caption: "Webアプリの場合",
+					children: [
+						{id: 1, type: 4, caption: "ブラウザ"},
+						{id: 2, type: 4, caption: "ブラウザの\n開発者ツール"},
+						{id: 3, type: 4, caption: "DBMSの\nクライアントツール"},
+						{id: 4, type: 4, caption: "Linux"},
+						{id: 5, type: 4, caption: "httpサーバ"},
+						{id: 6, type: 4, caption: "Sassコンパイラ"},
+						{id: 7, type: 4, caption: "OWASP Zap"},
+						{id: 8, type: 4, caption: "クラウドサービス"},
+						{id: 9, type: 5, caption: "http"},
+						{id: 10, type: 5, caption: "HTML"},
+						{id: 11, type: 5, caption: "HTMLテンプレート"},
+						{id: 12, type: 5, caption: "css, sass"},
+						{id: 13, type: 5, caption: "JavaScript"},
+						{id: 14, type: 5, caption: "DBMS、ORM"},
+						{id: 15, type: 5, caption: "認証、認可\nセッション"},
+						{id: 16, type: 5, caption: "セキュリティ"},
+						{id: 17, type: 6, caption: "SQL"},
+						{id: 18, type: 6, caption: "jQuery"},
+						{id: 19, type: 6, caption: "Bootstrap"},
+					]},
+			]},
+		{id: 2, type: 7, caption: "各プログラミング言語",
+			children: [
+				{id: 1, type: 8, caption: "文法を学ぶ"},
+				{id: 2, type: 8, caption: "定番ツールを\n使いこなす"},
+				{id: 3, type: 8, caption: "標準/外部ライブラリの\n使い方を学ぶ"},
+				{id: 4, type: 8, caption: "設計スタイルや\nイディオムを学ぶ"},
+			]},
+	];
+
+	let data = {nodes: [], edges: []};
+	const pushToData = (node, parentId)=>{
+		let id = parentId ? parentId+'-'+node.id : ''+node.id;
+
+		data.nodes.push({
+			id: id,
+			type: node.type,
+			caption: node.caption
+		});
+		if ( parentId ) data.edges.push([parentId, id])
+
+		if ( !node.children ) return;
+		node.children.forEach(n=>pushToData(n, id));
 	};
+	trees.forEach(tree=>pushToData(tree, null));
 
 	data.nodes.forEach(n=>{
 		chin.sys.addNode(n.id, n);
